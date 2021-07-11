b431eca89442cf3d786e1d35853c0ddb86a354d5
Andrzej Trzaska <atrzaska2@gmail.com>
2021-07-11 09:34:26 +0200

    Add PrivateRoute and PublicRoute components

---
+++ src/components/routes/PrivateRoute.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { getAccessToken } from 'src/services/jwt'
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const isLoggedIn = !!getAccessToken()
+
+  return (
+    // Show the component only when the user is logged in
+    // Otherwise, redirect the user to /signin page
+    <Route
+      {...rest}
+      render={(props) =>
+        isLoggedIn ? <Component {...props} /> : <Redirect to="/signin" />
+      }
+    />
+  )
+}
+
+export default PrivateRoute
