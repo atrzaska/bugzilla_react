27bf7a5e0aee550438dd9e45d705eae5b1ba05f7
Andrzej Trzaska <atrzaska2@gmail.com>
2021-07-11 09:33:29 +0200

    Add layout to dashboard

--- src/views/Dashboard/Index.js
+++ src/views/Dashboard/Index.js
@@ -1,5 +1,11 @@
+import AppLayout from 'src/layouts/App'
+
 const Dashboard = () => {
-  return <h1>Dashboard</h1>
+  return (
+    <AppLayout>
+      <h5>Dashboard</h5>
+    </AppLayout>
+  )
 }
 
 export default Dashboard
