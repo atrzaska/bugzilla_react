0c20a764c03439efb0c04cb36d1b7a18e93e3118
Andrzej Trzaska <atrzaska2@gmail.com>
2021-07-11 21:57:07 +0200

    Add more methods to useArray, rename state to value

--- src/hooks/useArray.js
+++ src/hooks/useArray.js
@@ -1,19 +1,15 @@
 import { useState } from 'react'
 
 const useArray = (initialState = []) => {
-  const [state, setState] = useState(initialState)
+  const [value, setValue] = useState(initialState)
 
-  const splice = (index) => {
-    setState((state) => state.filter((x, i) => i !== index))
-  }
+  const splice = (index) =>
+    setValue((value) => value.filter((x, i) => i !== index))
+  const remove = (item) => setValue((value) => value.filter((x) => x !== item))
+  const push = (item) => setValue((value) => [...value, item])
+  const append = (data) => setValue((value) => value.concat(data))
 
-  const remove = (item) => {
-    setState((state) => state.filter((x) => x !== item))
-  }
-
-  const push = (item) => setState((state) => [...state, item])
-
-  return { state, setState, splice, remove, push }
+  return { value, setValue, splice, remove, push, append }
 }
 
 export default useArray
