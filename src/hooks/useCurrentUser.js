75d4da75ff9d0f350ac16a9abc064c647da2ac2a
Andrzej Trzaska <atrzaska2@gmail.com>
2021-07-11 21:57:47 +0200

    Use setValue in useCurrentUser

--- src/hooks/useCurrentUser.js
+++ src/hooks/useCurrentUser.js
@@ -1,18 +1,18 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react'
-import useObject from 'src/hooks/useObject'
+import useObject from 'src/hooks/async/useObject'
 import API from 'src/services/requests'
 
 const useCurrentUser = (defaultValue = {}) => {
-  const { object: user, setObject, loading } = useObject(defaultValue)
+  const { object: user, loading, setValue } = useObject(defaultValue)
 
   useEffect(() => {
     API.fetchCurrentUser()
-      .then((res) => setObject(res.data))
-      .catch(() => setObject(defaultValue))
+      .then((res) => setValue(res.data))
+      .catch(() => setValue(defaultValue))
   }, [])
 
-  const isLoggedIn = () => !!user.id
+  const isLoggedIn = () => !!user.value.id
 
   return { isLoggedIn, user, loading }
 }
