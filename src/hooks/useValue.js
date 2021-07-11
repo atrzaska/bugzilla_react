3ba2e7280aa968a8aca1baeeb330a7a3667e9264
Andrzej Trzaska <atrzaska2@gmail.com>
2021-07-11 09:36:39 +0200

    Add hook for storing data objects

---
+++ src/hooks/useValue.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const getFieldAndValue = (e) => {
+  const target = e.target
+  const isCheckbox = target.type === 'checkbox'
+  let value = isCheckbox ? target.checked : target.value
+  const field = target.id
+  return { field, value }
+}
+
+const useValue = (initialState = {}) => {
+  const [value, setValue] = useState(initialState)
+
+  const setField = (field, value) => {
+    setValue((state) => {
+      const obj = { ...state }
+
+      if (value == null) {
+        delete obj[field]
+      } else {
+        obj[field] = value
+      }
+
+      return obj
+    })
+  }
+
+  const handleInputEvent = (e) => {
+    const { field, value } = getFieldAndValue(e)
+
+    setField(field, value)
+  }
+
+  return { value, handleInputEvent, setField, setValue }
+}
+
+export default useValue
