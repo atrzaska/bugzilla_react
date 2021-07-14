0487f23d21de5ef077dfba01b489e22ea4485c31
Andrzej Trzaska <atrzaska2@gmail.com>
2021-07-14 23:01:32 +0200

    Implement project edit and project new

--- src/views/Projects/Edit.js
+++ src/views/Projects/Edit.js
@@ -1,9 +1,53 @@
 import AppLayout from 'src/layouts/App'
+import Loading from 'src/components/Loading'
+import Field from 'src/components/form/Field'
+import FormButtons from 'src/components/form/FormButtons'
+import useEditForm from 'src/hooks/useEditForm'
+import { projectSchema as schema } from 'src/services/yup'
+import API from 'src/services/requests'
+import useObject from 'src/hooks/async/useObject'
 
 const ProjectsEdit = () => {
+  const object = useObject({ name: '' })
+  const { value: data, handleInputEvent } = object
+  const { isSubmitting, isValid, loading, onSubmit, validation } = useEditForm({
+    object,
+    schema,
+    onFetch: (id) => API.fetchProject(id),
+    onUpdate: (id, data) => API.updateProject(id, data),
+    successToast: (data) => `Project ${data.name} updated successfully.`,
+    successRedirectPath: '/projects',
+  })
+
+  const onSubmitWrapped = (e) => {
+    e.preventDefault()
+    onSubmit()
+  }
+
   return (
     <AppLayout>
-      <h1>ProjectsEdit</h1>
+      <h1 className="mb-4">Edit Project</h1>
+      {loading ? (
+        <Loading />
+      ) : (
+        <form onSubmit={onSubmitWrapped}>
+          <Field
+            v-model="data.name"
+            value={data.name}
+            onChange={handleInputEvent}
+            validation={validation}
+            id="name"
+            label="Name"
+            autofocus
+          />
+          <hr />
+          <FormButtons
+            isValid={isValid}
+            isSubmitting={isSubmitting}
+            backLink="/projects"
+          />
+        </form>
+      )}
     </AppLayout>
   )
 }
--- src/views/Projects/New.js
+++ src/views/Projects/New.js
@@ -1,9 +1,45 @@
 import AppLayout from 'src/layouts/App'
+import useNewForm from 'src/hooks/useNewForm'
+import Field from 'src/components/form/Field'
+import FormButtons from 'src/components/form/FormButtons'
+import { projectSchema as schema } from 'src/services/yup'
+import API from 'src/services/requests'
+import useValue from 'src/hooks/useValue'
 
 const ProjectsNew = () => {
+  const { value: data, handleInputEvent } = useValue({ name: '' })
+  const { isValid, isSubmitting, onSubmit, validation } = useNewForm({
+    data,
+    schema,
+    onCreate: (data) => API.createProject(data),
+    successToast: (data) => `Project ${data.name} created successfully.`,
+    successRedirectPath: '/projects',
+  })
+
+  const onSubmitWrapper = (e) => {
+    e.preventDefault()
+    onSubmit()
+  }
+
   return (
     <AppLayout>
-      <h1>ProjectsNew</h1>
+      <h1 className="mb-4">New Project</h1>
+      <form onSubmit={onSubmitWrapper}>
+        <Field
+          value={data.name}
+          onChange={handleInputEvent}
+          validation={validation}
+          id="name"
+          label="Name"
+          autofocus
+        />
+        <hr />
+        <FormButtons
+          isValid={isValid}
+          isSubmitting={isSubmitting}
+          backLink="/projects"
+        />
+      </form>
     </AppLayout>
   )
 }
