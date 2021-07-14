48eb4459b044ee8fbe4d95b77fd43aae8782967b
Andrzej Trzaska <atrzaska2@gmail.com>
2021-07-14 23:00:27 +0200

    Use external object for data in useEditForm

--- src/hooks/useEditForm.js
+++ src/hooks/useEditForm.js
@@ -2,12 +2,12 @@ import { history } from 'src/router'
 import { useParams } from 'react-router-dom'
 import useForm from 'src/hooks/useForm'
 import useFrontendValidation from 'src/hooks/useFrontendValidation'
-import useObject from 'src/hooks/async/useObject'
 import useValue from 'src/hooks/useValue'
+import { useEffect } from 'react'
 
 const useEditForm = ({
   id,
-  data,
+  object,
   schema,
   onFetch,
   onFetchError,
@@ -16,16 +16,16 @@ const useEditForm = ({
   successToast,
   successRedirectPath,
 }) => {
+  const { value: data } = object
   const errors = useValue({})
   const form = useForm({ errors })
   const validation = useFrontendValidation({ data, errors, schema })
-  const object = useObject(data)
   const urlParams = useParams()
   id = id || urlParams.id
 
   const onSubmit = () => {
     validation.validateForm() &&
-      form.submit(onUpdate(id, data.value)).then((res) => {
+      form.submit(onUpdate(id, data)).then((res) => {
         successRedirectPath && history.push(successRedirectPath)
         successToast && window.Toast.success(successToast(res.data))
         onSuccess && onSuccess(res.data)
@@ -39,7 +39,8 @@ const useEditForm = ({
       .catch((err) => onFetchError(err))
   }
 
-  fetchObject()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(fetchObject, [])
 
   return {
     ...validation,
