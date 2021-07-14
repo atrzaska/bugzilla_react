e1565c1df93f36ea4406e5badd9db648e8151e60
Andrzej Trzaska <atrzaska2@gmail.com>
2021-07-14 12:02:29 +0200

    Add handleInputEvent for useSorting

--- src/hooks/useSorting.js
+++ src/hooks/useSorting.js
@@ -1,12 +1,14 @@
-import QueryParams from '@/services/QueryParams'
+import QueryParams from 'src/services/QueryParams'
 import { useEffect, useState } from 'react'
 
 const useSorting = (initialValue = '') => {
   const [sort, setSort] = useState(QueryParams.get('sort') || initialValue)
 
+  const handleInputEvent = (e) => setSort(e.target.value)
+
   useEffect(() => QueryParams.set({ sort }), [sort])
 
-  return { sort, setSort }
+  return { sort, setSort, handleInputEvent }
 }
 
 export default useSorting
