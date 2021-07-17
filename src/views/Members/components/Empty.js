fd9415c19d33ca47ba8af5b981f689da072ea3ba
Andrzej Trzaska <atrzaska2@gmail.com>
2021-07-17 23:56:43 +0200

    Fix className attr

--- src/views/Members/components/Empty.js
+++ src/views/Members/components/Empty.js
@@ -6,11 +6,11 @@ const Empty = () => {
 
   return (
     <React.Fragment>
-      <h1 class="text-center text-secondary p-4">
+      <h1 className="text-center text-secondary p-4">
         This project does not have any members yet
       </h1>
-      <div class="text-center">
-        <Link class="btn btn-primary" to={`/projects/${id}/members`}>
+      <div className="text-center">
+        <Link className="btn btn-primary" to={`/projects/${id}/members`}>
           Create member
         </Link>
       </div>
