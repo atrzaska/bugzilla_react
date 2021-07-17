168e720644945b521896fd5e8d359aecd2bc18be
Andrzej Trzaska <atrzaska2@gmail.com>
2021-07-17 09:03:54 +0200

    Fix issues with comments edit page

--- src/views/Comments/Edit.js
+++ src/views/Comments/Edit.js
@@ -7,17 +7,20 @@ import useEditForm from 'src/hooks/useEditForm'
 import { commentSchema as schema } from 'src/services/yup'
 import API from 'src/services/requests'
 import QueryParams from 'src/services/QueryParams'
+import useObject from 'src/hooks/async/useObject'
 
 const CommentsEdit = () => {
   const backPath = QueryParams.get('back') || '/dashboard'
-  const { data, isSubmitting, isValid, loading, onSubmit, validation } =
-    useEditForm({
-      schema,
-      onFetch: (id) => API.fetchComment(id),
-      onUpdate: (id, data) => API.updateComment(id, data),
-      successToast: (data) => 'Comment updated successfully.',
-      successRedirectPath: backPath,
-    })
+  const object = useObject({ content: '' })
+  const { value: data, handleInputEvent } = object
+  const { isSubmitting, isValid, loading, onSubmit, validation } = useEditForm({
+    object,
+    schema,
+    onFetch: (id) => API.fetchComment(id),
+    onUpdate: (id, data) => API.updateComment(id, data),
+    successToast: (data) => 'Comment updated successfully.',
+    successRedirectPath: backPath,
+  })
 
   return (
     <AppLayout>
@@ -28,6 +31,7 @@ const CommentsEdit = () => {
         <Form onSubmit={onSubmit}>
           <TextArea
             value={data.content}
+            onChange={handleInputEvent}
             validation={validation}
             id="content"
             label="Content"
